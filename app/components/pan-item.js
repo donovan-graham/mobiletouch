da26ffd3a0041f708115b08fc7f019b6ee4125a6
Donovan Graham <donovan@platform7.com>
2015-01-22 12:02:08 +0200

    better comments

--- app/components/pan-item.js
+++ app/components/pan-item.js
@@ -12,6 +12,11 @@ export default Ember.Component.extend({
   isOpen: false,
   hammer: null, 
 
+  transitionNone: '-webkit-transition: none; -moz-transition: none; -ms-transition: none; -o-transition: none; transition: none;',
+  transitionAnimate: '-webkit-transition: -webkit-transform %@1; -moz-transition: -moz-transform %@1; -ms-transition: -ms-transform %@1; -o-transition: -o-transform %@1; transition: transform %@1;',
+  transformX: '-webkit-transform: translate3d(%@1px,0px,0px) scale3d(1,1,1); -moz-transform: translate3d(%@1px,0px,0px); -ms-transform: translate3d(%@1px,0px,0px); -o-transform: translate3d(%@1px,0px,0px); transform: translate3d(%@1px,0px,0px);',
+
+
 
   _setupCoordinates: function() {
     this.get('revealClip');
@@ -30,8 +35,8 @@ export default Ember.Component.extend({
     
     var self = this;
     
-    hammer.on('panleft', function (ev) {
-      var output = self.panLeft.apply(self, Array.prototype.slice.call(arguments));
+    hammer.on('panleft panright', function (ev) {
+      var output = self.panHorizontal.apply(self, Array.prototype.slice.call(arguments));
       if (output === false) {
         if (typeof event.stopPropagation !== 'undefined') {
           event.stopPropagation();
@@ -42,19 +47,17 @@ export default Ember.Component.extend({
       return output;
     });
 
-
-    
-    hammer.on('panright', function (ev) {
-      var output = self.panRight.apply(self, Array.prototype.slice.call(arguments));
-      if (output === false) {
-        if (typeof event.stopPropagation !== 'undefined') {
-          event.stopPropagation();
-        } else {
-          event.srcEvent.stopPropagation();
-        }
-      }
-      return output;
-    });
+    // hammer.on('panright', function (ev) {
+    //   var output = self.panRight.apply(self, Array.prototype.slice.call(arguments));
+    //   if (output === false) {
+    //     if (typeof event.stopPropagation !== 'undefined') {
+    //       event.stopPropagation();
+    //     } else {
+    //       event.srcEvent.stopPropagation();
+    //     }
+    //   }
+    //   return output;
+    // });
 
 
     hammer.on('panend', function (ev) {
@@ -92,90 +95,124 @@ export default Ember.Component.extend({
   }.on('willDestroyElement'),
 
   revealClip: function() {
-    return Math.round((this.get('revealWidth') / 2),0);  
+    return Math.round((this.get('revealWidth') / 2), 0);  
   }.property('revealWidth'),
 
 
-  panLeft: function(ev) {
+  // panLeft: function(ev) {
+  //   this.panStart(); 
+  //   this.animateHorizontalPan(this.startX + ev.deltaX);
+  // },
 
+  // panRight: function(ev) {
+  //   this.panStart(); 
+  //   this.animateHorizontalPan(this.startX + ev.deltaX);
+  // },
+
+
+  panStart: function() {
     if (this.startX === null || this.startX === undefined) { 
       this.startX = this.$().position().left; 
     }
+  },
 
-    // console.log("panLeft:", ev.deltaX, " startX:", this.startX);
+  panHorizontal: function(ev) {
+    /* 
+    hammer2 fires panStart only after the first panLeft panRight.  
+    So we need to manage the pan start manually. 
+    */
+    this.panStart(); 
     this.animateHorizontalPan(this.startX + ev.deltaX);
   },
 
-  panRight: function(ev) {
-    if (this.startX === null || this.startX === undefined) { 
-      this.startX = this.$().position().left; 
-    }
-
-    // console.log("panRight:", ev.deltaX, " el:", this.get('startX'));
-    this.animateHorizontalPan(this.startX + ev.deltaX);
-  },
 
   panEnd: function(ev) {
-
     var absX = Math.abs(this.lastX);
-
-    if (absX === this.get('revealWidth') || absX === 0) { return; }
+    this.startX = null; 
 
     if (absX >= this.get('revealClip')) {
-      this.set('isOpen', true);
-
+       this.set('isOpen', true);
     } else {
       this.set('isOpen', false);
     }
 
+    if (absX === this.get('revealWidth') || absX === 0) { return; }
+
     this.animateHorizontalSlide();
-    this.startX = null; 
   },
 
 
   animateHorizontalPan: function(xPos) {
     xPos = Math.min(Math.max(xPos, -1 * this.get('revealWidth')), 0);
-
+ 
+    if (this.lastX === xPos) { return; }  // don't need to make any new changes
+ 
     this.lastX = xPos;
 
+    /*
+    // Test 1
     var style = [];
+ 
+    style.push('-webkit-transition: none');
+    style.push('-moz-transition: none');
+    style.push('-ms-transition: none');
+    style.push('-o-transition: none');
+    style.push('transition: none');
+     
+    style.push('-webkit-transform: translate3d(' + xPos + 'px,0px,0px) scale3d(1,1,1)');
+    style.push('-moz-transform: translate3d(' + xPos + 'px,0px,0px)');
+    style.push('-ms-transform: translate3d(' + xPos + 'px,0px,0px)');
+    style.push('-o-transform: translate3d(' + xPos + 'px,0px,0px)');
+    style.push('transform: translate3d(' + xPos + 'px,0px,0px)');
+ 
+    this.$().attr("style", style.join("; "));
+    return;
+    */
 
+    /*
+    // Test 2
+    var style = this.$()[0].style;   
+    style.webkitTransition = 'none';
+    style.mozTransition = 'none';
+    style.msTransition = 'none';
+    style.oTransition = 'none';
+    style.transition = 'none';
+    style.webkitTransform = 'translate3d(' + xPos + 'px,0px,0px) scale3d(1,1,1)';
+    style.mozTransform = 'translate3d(' + xPos + 'px,0px,0px)';
+    style.msTransform = 'translate3d(' + xPos + 'px,0px,0px)';
+    style.oTransform = 'translate3d(' + xPos + 'px,0px,0px)';
+    style.transform = 'translate3d(' + xPos + 'px,0px,0px)';
+    
+    this.$()[0].style = style;
+    return;
+    */
+
+    
+    /*
+    // Test 3
     var transitionNone = '-webkit-transition: none; -moz-transition: none; -ms-transition: none; -o-transition: none; transition: none;',
         transform =  '-webkit-transform: translate3d(%@1px,0px,0px) scale3d(1,1,1); -moz-transform: translate3d(%@1px,0px,0px); -ms-transform: translate3d(%@1px,0px,0px); -o-transform: translate3d(%@1px,0px,0px); transform: translate3d(%@1px,0px,0px);';
+    this.$().attr("style", [transitionNone, transform.fmt(xPos)].join(" "));
+    */
 
-    // Test 1
-    // var style = this.$()[0].style;   
-    // style.webkitTransition = 'none';
-    // style.mozTransition = 'none';
-    // style.msTransition = 'none';
-    // style.oTransition = 'none';
-    // style.transition = 'none';
-    // style.webkitTransform = 'translate3d(' + xPos + 'px,0px,0px) scale3d(1,1,1)';
-    // style.mozTransform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // style.msTransform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // style.oTransform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // style.transform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // this.$()[0].style = style;
-
-    // Test 2
-    // this.$().attr("style", [transitionNone, transform.fmt(xPos)].join(" "));
-
-    // Test 3
-    this.$()[0].style.cssText = [transitionNone, transform.fmt(xPos)].join(" ");
-
+     // Test 4
+    this.$()[0].style.cssText = [this.transitionNone, this.transformX.fmt(xPos)].join(" ");
+    return;
   },
 
 
   animateHorizontalSlide: function() {
 
-    var xPos,
+     var xPos,
         relativeDuration,
-        animation = 'ease-out',
-        transitionNone = '-webkit-transition: none; -moz-transition: none; -ms-transition: none; -o-transition: none; transition: none;',
-        transition = '-webkit-transition: -webkit-transform %@1; -moz-transition: -moz-transform %@1; -ms-transition: -ms-transform %@1; -o-transition: -o-transform %@1; transition: transform %@1;',
-        transform =  '-webkit-transform: translate3d(%@1px,0px,0px) scale3d(1,1,1); -moz-transform: translate3d(%@1px,0px,0px); -ms-transform: translate3d(%@1px,0px,0px); -o-transform: translate3d(%@1px,0px,0px); transform: translate3d(%@1px,0px,0px);';
+        animation = 'ease-out';
 
     xPos = (this.get('isOpen')) ? -1 * this.get('revealWidth') : 0;
+
+    xPos = Math.min(Math.max(xPos, -1 * this.get('revealWidth')), 0);
+ 
+    if (this.lastX === xPos) { return; }  // don't need to make any new changes
+ 
 
     // var speed = this.get('revealClip') / this.get('duration'); // pixels per millisecond
     // var distance = Math.abs(xPos - this.lastX);
@@ -186,35 +223,40 @@ export default Ember.Component.extend({
     relativeDuration =  Math.abs(xPos - this.lastX) / (this.get('revealClip') / this.get('duration'));
 
 
+    /*
     // Test 1
-    // var style = this.$()[0].style;   
-    // style.webkitTransition = 'transform ' + relativeDuration + 'ms ' + animation;
-    // style.mozTransition = 'transform ' + relativeDuration + 'ms ' + animation;
-    // style.msTransition = 'transform ' + relativeDuration + 'ms ' + animation;
-    // style.oTransition = 'transform ' + relativeDuration + 'ms ' + animation;
-    // style.transition = 'transform ' + relativeDuration + 'ms ' + animation;
-    // style.webkitTransform = 'translate3d(' + xPos + 'px,0px,0px) scale3d(1,1,1)';
-    // style.mozTransform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // style.msTransform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // style.oTransform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // style.transform = 'translate3d(' + xPos + 'px,0px,0px)';
-    // this.$()[0].style = style;
-    
+    var style = this.$()[0].style;   
+    style.webkitTransition = 'transform ' + relativeDuration + 'ms ' + animation;
+    style.mozTransition = 'transform ' + relativeDuration + 'ms ' + animation;
+    style.msTransition = 'transform ' + relativeDuration + 'ms ' + animation;
+    style.oTransition = 'transform ' + relativeDuration + 'ms ' + animation;
+    style.transition = 'transform ' + relativeDuration + 'ms ' + animation;
+    style.webkitTransform = 'translate3d(' + xPos + 'px,0px,0px) scale3d(1,1,1)';
+    style.mozTransform = 'translate3d(' + xPos + 'px,0px,0px)';
+    style.msTransform = 'translate3d(' + xPos + 'px,0px,0px)';
+    style.oTransform = 'translate3d(' + xPos + 'px,0px,0px)';
+    style.transform = 'translate3d(' + xPos + 'px,0px,0px)';
+    this.$()[0].style = style;
+    */
+
+    /*
     // Test 2
-    // this.$().attr("style", [transition.fmt(relativeDuration +'ms '+ animation), transform.fmt(xPos)].join(" "));
+
+    var transitionNone = '-webkit-transition: none; -moz-transition: none; -ms-transition: none; -o-transition: none; transition: none;',
+        transitionAnimate = '-webkit-transition: -webkit-transform %@1; -moz-transition: -moz-transform %@1; -ms-transition: -ms-transform %@1; -o-transition: -o-transform %@1; transition: transform %@1;',
+        transformX =  '-webkit-transform: translate3d(%@1px,0px,0px) scale3d(1,1,1); -moz-transform: translate3d(%@1px,0px,0px); -ms-transform: translate3d(%@1px,0px,0px); -o-transform: translate3d(%@1px,0px,0px); transform: translate3d(%@1px,0px,0px);';
+
+    this.$().attr("style", [transition.fmt(relativeDuration +'ms '+ animation), transform.fmt(xPos)].join(" "));
+    return;
+    */
 
     // Test 3
-    this.$()[0].style.cssText = [transition.fmt(relativeDuration +'ms '+ animation), transform.fmt(xPos)].join(" ");
-
-    // if (!this.get('isOpen')) {
-    //   Ember.run.later(this, function() {
-    //     this.$().attr("style", "");
-    //   }, relativeDuration + 1);
-    // } else {
-    //   Ember.run.later(this, function() {
-    //     this.$().attr("style", [transitionNone, transform.fmt(xPos)].join(" "));
-    //   }, relativeDuration + 1);
-    // }
+    this.$()[0].style.cssText = [this.transitionAnimate.fmt(relativeDuration +'ms '+ animation), this.transformX.fmt(xPos)].join(" ");
+ 
+    // Ember.run.later(this, function() {
+    //   this.$().attr("style", [transitionNone, transform.fmt(xPos)].join(" "));
+    // }, relativeDuration + 1);
+    
   }
 
 
