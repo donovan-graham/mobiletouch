a7e7882d9370a53c5eb9d479fa9323546b81cd49
Donovan Graham <donovan@platform7.com>
2015-02-03 11:10:05 +0200

    Adding X,Y,X coords to transform3D.  Adding performant bulk edit to lists

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  items: Ember.computed.alias('model')
+  items: Ember.computed.alias('model'),
+
+  actions: {
+    toggleEditMode: function() {
+      Ember.$('.list-container')[0].classList.toggle('is-checking');
+      return;
+    }
+
+  }
+
 });
