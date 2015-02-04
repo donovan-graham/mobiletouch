0fd01ff31ab0b8a5072fc0f3f23c9e20f5ba88fa
Donovan Graham <donovan@platform7.com>
2015-02-04 17:10:07 +0200

    Fastclick makes elements clickable on IOS.  Font-size renders differently on mobile than desktop, so need to update header-menu itemHeights accordingly

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/hammerjs/hammer.js');
+app.import('bower_components/fastclick/lib/fastclick.js');
 
 module.exports = app.toTree();
