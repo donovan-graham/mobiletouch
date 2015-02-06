70857d155530b95338ee31fd9384d73ae95dc51c
Michiel <vanwijk.mc@gmail.com>
2015-02-06 10:18:09 +0200

    Added drag-list for new gesture dev

---
+++ app/components/drag-item.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['list-item-content', 'no-select'],
+
+
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route("list", { path: "/" });
+  this.route("drag-list", { path: "drag-list" });
 });
 
 export default Router;
