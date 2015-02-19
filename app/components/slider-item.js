7084a90fc53a2e5de7941f52d9088e5ee65bcc1b
Michiel <vanwijk.mc@gmail.com>
2015-02-19 12:11:22 +0200

    Initial value can now be passed in and percentage calculation works in both directions

--- app/components/slider-item.js
+++ app/components/slider-item.js
@@ -3,23 +3,38 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNameBindings: [':no-select', ':slider-item'],
 
-  isSliding: false,
-
   hammer: null,
 
-  sliderContainer: null,
   sliderHandle: null,
   sliderWidth: 0,
 
   rafPanId: null,
   rafSlideId: null,
+  rafPercId: null,
+
+  increment: null,
+  percentage: 100,
+  trackedPercentage: 0,
+
+  deltaX: null,
 
   _setup: function() {
 
-    this.sliderContainer = this.get('element');
-    this.sliderHandle = this.get('element').childNodes[1].childNodes[1].childNodes[1]; // specificity for speed
-    this.sliderWidth = this.get('element').childNodes[1].offsetWidth;
+    // define all elements
+    this.sliderHandle = this.get('element').getElementsByClassName('handle')[0];
+    this.sliderFill = this.get('element').getElementsByClassName('filler')[0];
+    this.sliderWidth = this.get('element').getElementsByClassName('progress-bar')[0].offsetWidth;
 
+    // get pixel-to-percentage ratio
+    this.increment = 100 / this.sliderWidth;
+
+    this.set('trackedPercentage', this.get('percentage'));
+
+    // set start position
+    this.lastX = this.get('percentage') / this.increment;
+    this.animateHorizontalPan();
+
+    // ini hammer bindings
     var hammer = new Hammer.Manager(this.get('element'));
       hammer.add(new Hammer.Pan({ enable: true, direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10 }));
 
@@ -45,17 +60,12 @@ export default Ember.Component.extend({
 
   panStart: function(event) {
 
-    this.set('isSliding', true);
-    console.log('>>>> panStart');
+    // set limits
+    this.leftLimit = 0;
+    this.rightLimit = this.sliderWidth;
 
-    var style = window.getComputedStyle(this.sliderHandle);  // Need the DOM object
-    var matrix = new WebKitCSSMatrix(style.webkitTransform);
-    this.startX = matrix.m41;
-    this.startY = matrix.m42;
-    this.startZ = matrix.m43;
-
-    this.leftLimit = (this.startX + this.sliderWidth) * -1;
-    this.rightLimit = this.sliderWidth - (this.startX + this.sliderWidth);
+    // set new start position
+    this.startX = this.lastX;
 
     if (this.rafSlideId) {
       window.cancelAnimationFrame(this.rafSlideId);
@@ -64,40 +74,80 @@ export default Ember.Component.extend({
     
   },
 
+  trackPercentage: function() {
+    this.rafPercId = null;
+
+    var newPercentage = this.get('percentage') + (this.deltaX * this.increment);
+
+    console.log('this.deltaX', this.deltaX, 'decrement', this.deltaX * this.increment, 'percentage', this.get('percentage'), newPercentage);
+    if (newPercentage > 100) { newPercentage = 100; }
+    if (newPercentage < 0) { newPercentage = 0; }
+
+    this.set('trackedPercentage', Math.round(newPercentage));
+
+  },
+
   panMove: function(event) {
 
-      var newX = event.deltaX + this.startX; 
+    // console.log('initialHandlePosition', this.initialHandlePosition, 'increment', this.increment)
+    this.deltaX = event.deltaX;
 
-      if (event.deltaX < this.leftLimit){ 
-        this.lastX = this.sliderWidth * -1;
-        if (!this.rafPanId) {
-          this.rafPanId = window.requestAnimationFrame(this.animateHorizontalPan.bind(this));
-        }
-        return;
-      }
-      if (event.deltaX > this.rightLimit){ 
-        this.lastX = 0;
-        if (!this.rafPanId) {
-          this.rafPanId = window.requestAnimationFrame(this.animateHorizontalPan.bind(this));
-        }
-        return; 
-      }
+    // console.log(this.lastX)
+
+    if (!this.rafPercId) {
+      this.rafPercId = window.requestAnimationFrame(this.trackPercentage.bind(this));
+    }
+
+      
+    var newX = this.deltaX + this.startX; 
+
+    // console.log(newX);
+      
+    if (newX < this.leftLimit) { newX = this.leftLimit; }
+    if (newX > this.rightLimit) { newX = this.rightLimit; } 
+
+      // console.log('newX', newX);
 
       this.lastX = newX;
 
       if (!this.rafPanId) {
         this.rafPanId = window.requestAnimationFrame(this.animateHorizontalPan.bind(this));
       }
+  
+
+    // if (event.deltaX < this.leftLimit){ 
+    //   this.lastX = this.sliderWidth * -1;
+    //   if (!this.rafPanId) {
+    //     this.rafPanId = window.requestAnimationFrame(this.animateHorizontalPan.bind(this));
+    //   }
+    //   return;
+    // }
+    // if (event.deltaX > this.rightLimit){ 
+    //   this.lastX = 0;
+    //   if (!this.rafPanId) {
+    //     this.rafPanId = window.requestAnimationFrame(this.animateHorizontalPan.bind(this));
+    //   }
+    //   return; 
+    // }
+
+    // this.lastX = newX;
+
+    // if (!this.rafPanId) {
+    //   this.rafPanId = window.requestAnimationFrame(this.animateHorizontalPan.bind(this));
+    // }
     
   },
 
   panEnd: function() {
     console.log('>>>> panEnd');
-    this.set('isSliding', false);
+    this.set('percentage', this.get('trackedPercentage'));
   },
 
   animateHorizontalPan: function() {
     this.rafPanId = null;      // release the lock
+
+    this.startY = 0;
+    this.startZ = 0;
 
     var newX = this.lastX,
         style = '';
@@ -115,6 +165,7 @@ export default Ember.Component.extend({
     style += 'transform: translate3d(' + newX + 'px,' + this.startY + 'px,' + this.startZ + 'px); ';
 
     this.sliderHandle.style.cssText = style;
+    this.sliderFill.style.cssText = style;
   }
 
 });
